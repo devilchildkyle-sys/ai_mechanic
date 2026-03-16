2c798f27e0be46deeb0a265ae5dd721b243d0322
devilchildkyle-sys <devilchildkyle@gmail.com>
2026-03-16 12:51:30 -0600

    Add files via upload

---
+++ config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const String awsAccessKey = 'YOUR_AWS_ACCESS_KEY';
+  static const String awsSecretKey = 'YOUR_AWS_SECRET_KEY';
+  static const String awsRegion = 'us-east-1';
+}
